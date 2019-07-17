0732f9c575e88fa5eef61908597cc46c99662b87
FredericMentel <frederic.mentel@gmail.com>
2019-07-17 11:48:00 +0200

    Integration Cirque Page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,63 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div class="container-fluid home ">
-                <Navbar />
-            <div className="container-history p-3">Home</div>
+  render() {
+    return (
+      <div className="container-fluid home ">
+        <Navbar />
+        <div className="p-3 d-flex">
+          <div className="container-img">
+            <img
+              src={require("../family-circus.jpg")}
+              alt="famille"
+              className="img-family"
+            />
+          </div>
+          <div className="container-history-text p-5 d-flex">
+            <div>
+              <h3>Notre Histoire</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent vitae sodales ex, at condimentum sem. Fusce et molestie
+                urna. Curabitur placerat arcu sed augue malesuada, sed pulvinar
+                enim efficitur. Nulla accumsan, diam ut tristique feugiat, nisl
+                elit semper risus, eu pellentesque diam libero vitae eros. Ut
+                feugiat mi non quam commodo, a porttitor mauris pellentesque.
+                Proin in nibh cursus, tempus lorem ac, iaculis erat. Fusce
+                maximus tristique velit id venenatis.
+              </p>
             </div>
-        )
-    }
+          </div>
+        </div>
+        <div className="p-3 d-flex">
+          <div className="container-history-text p-5 d-flex">
+            <div>
+              <h3>Nos installations</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent vitae sodales ex, at condimentum sem. Fusce et molestie
+                urna. Curabitur placerat arcu sed augue malesuada, sed pulvinar
+                enim efficitur. Nulla accumsan, diam ut tristique feugiat, nisl
+                elit semper risus, eu pellentesque diam libero vitae eros. Ut
+                feugiat mi non quam commodo, a porttitor mauris pellentesque.
+                Proin in nibh cursus, tempus lorem ac, iaculis erat. Fusce
+                maximus tristique velit id venenatis.
+              </p>
+            </div>
+          </div>
+          <div className="container-img">
+            <img
+              src={require("../Russian_Circus_1.jpg")}
+              alt="famille"
+              className="img-family"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
